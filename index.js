ff891b86949c74f21a6d0509b0540b84f876daa4
Otabek <mavlonovo@tamu.edu>
2023-08-13 22:54:48 -0500

    updated handlers  and ejs

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 import express from "express";
+import bodyParser from "body-parser";
+
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs"); //what does this do?
 
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var taskList = ["Learn how to use ToDoish"];
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  //   res.render("index.ejs");
+  res.render("index", { task: taskList /*, complete: complete*/ }); //what does this do?
 });
 
 app.post("/addtask", (req, res) => {
-    res.render("index.ejs");
+  var currInput = req.body.newtask;
+  taskList.push(currInput);
+  res.redirect("/");
 });
 
 app.listen(port, () => {
