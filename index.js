0d797101eb98f74a22d729c7825c48968d7648ec
Otabek <mavlonovo@tamu.edu>
2023-08-13 23:44:36 -0500

    updated handlers

--- index.js
+++ index.js
@@ -9,15 +9,32 @@ app.set("view engine", "ejs"); //what does this do?
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var taskList = ["Learn how to use ToDoish"];
+var completedList = ["Water the rocks"];
 
 app.get("/", (req, res) => {
   //   res.render("index.ejs");
-  res.render("index", { task: taskList /*, complete: complete*/ }); //what does this do?
+  res.render("index", { task: taskList , complete: completedList }); //what does this do?
 });
 
 app.post("/addtask", (req, res) => {
   var currInput = req.body.newtask;
   taskList.push(currInput);
+  res.redirect("/");
+});
+
+app.post("/removetask", function (req, res) {
+  var completeTask = req.body.inputted;
+  //check for the "typeof" the different completed task, then add into the complete task
+  if (typeof completeTask === "string") {
+    completedList.push(completeTask);
+    //check if the completed task already exist in the task when checked, then remove using the array splice method
+    task.splice(task.indexOf(completeTask), 1);
+  } else if (typeof completeTask === "object") {
+    for (var i = 0; i < completeTask.length; i++) {
+      completedList.push(completeTask[i]);
+      task.splice(task.indexOf(completeTask[i]), 1);
+    }
+  }
   res.redirect("/");
 });
 
